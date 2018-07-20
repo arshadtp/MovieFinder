4b2cb0aa1d52acb78c1a80d7c7e5f2b916440141
Arshad <arshad@qb-ipl-1243.local>
2018-07-20 19:07:15 +0530

    Adding UI Tests

--- MovieFinderUITests/MovieFinderUITests.swift
+++ MovieFinderUITests/MovieFinderUITests.swift
@@ -9,28 +9,93 @@
 import XCTest
 
 class MovieFinderUITests: XCTestCase {
-        
-    override func setUp() {
-        super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+  
+  var app: XCUIApplication!
+  let validSearchTerm = "Batman"
+  let invalidSearchTerm = "adadadaadadfgtrbfryhfhfhfdfghdfgffdgdfhgdfsfsdgdsfdgd"
+  
+  override func setUp() {
+    super.setUp()
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    continueAfterFailure = false
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    app = XCUIApplication()
+    app.launch()
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  }
+  
+  override func tearDown() {
+    app = nil
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  // -------------------------
+  // MARK: - Tests
+  // -------------------------
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
+  func testMovieSearchSuccessScenario() {
+    tapOnSearchField()
+    enterSearchTerm(validSearchTerm)
+    tapSearchButton()
+    XCTAssert(waitForResultLoading(), "Data does not loaded correctly")
+  }
+  
+  func testMovieSearchFailureScenarioWithInvalidQuery() {
+    tapOnSearchField()
+    enterSearchTerm(invalidSearchTerm)
+    tapSearchButton()
+    XCTAssert(isErrorShowed(), "Alert didn't showed correctly")
+  }
+  
+  func testIfSearchQueryCachedOnSuccess()  {
+    tapOnSearchField()
+    enterSearchTerm("Ip Man")
+    tapSearchButton()
+    waitForResultLoading()
+    tapOnSearchField()
+    let isCached = app.tables["cache_list"].cells.firstMatch.staticTexts["Ip Man"].exists
+    XCTAssert(isCached, "Search didn't cached")
+  }
+  
+  func testIfSearchQueryCachedOnFailure()  {
+    tapOnSearchField()
+    enterSearchTerm(invalidSearchTerm)
+    tapSearchButton()
+    waitForResultLoading()
+    tapOnSearchField()
+    let isCached = app.tables["cache_list"].cells.firstMatch.staticTexts[invalidSearchTerm].exists
+    XCTAssert(!isCached, "Search term cached on failure")
+  }
+  // -------------------------
+  // MARK: - Helper methods
+  // -------------------------
+
+  @discardableResult
+  func waitForResultLoading() -> Bool {
+    return app.tables.cells.firstMatch.waitForExistence(timeout: 10)
+  }
+  
+  func isErrorShowed() -> Bool {
+    return app.alerts["Error"].waitForExistence(timeout: 10)
+  }
+  
+  func tapOnSearchField() {
+    let searchField = app.searchFields["Search Movies"]
+    searchField.tap()
+  }
+  
+  func enterSearchTerm(_ term: String)  {
+    let searchField = app.searchFields["Search Movies"]
+    searchField.typeText(term)
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
+  }
+  
+  func tapSearchButton() {
+    app/*@START_MENU_TOKEN@*/.buttons["Search"]/*[[".keyboards.buttons[\"Search\"]",".buttons[\"Search\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+  }
+  
 }
